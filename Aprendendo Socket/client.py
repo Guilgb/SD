c0e150d852c290b425a0f86dabdf3539c819e8c6
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-02-21 11:10:07 -0300

    ok

--- Aprendendo Socket/client.py
+++ Aprendendo Socket/client.py
@@ -12,8 +12,7 @@ image_data = file.read(2048)
 while image_data:
     client.send(image_data)
     image_data = file.read(2048)
-
-# file.close()
-# client.close()
+    file.close()
+    client.close()
 
 print("Mensagem ecoada: ", image_data.decode())
